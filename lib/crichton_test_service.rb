b80fcdc947e9ba6e717dba5c8e2a2383994362d4
Connor Savage <csavage@mdsol.com>
2014-11-21 12:49:50 -0500

    expose methods for intializing rails and spawning a rails process

--- lib/crichton_test_service.rb
+++ lib/crichton_test_service.rb
@@ -1,5 +1,32 @@
 require "crichton_test_service/version"
 
 module CrichtonTestService
-  # Your code goes here...
+  def self.initialize_rails!
+    require File.dirname(__FILE__) << "/crichton_test_service/config/environment"
+  end
+
+  # Uses Process.spawn to setup and spin up the crichton demo service rails server
+  # Waits
+  def self.spawn_rails_process!(port = 3000)
+    rails_root = File.dirname(__FILE__) << '/crichton_test_service'
+    Dir.chdir(rails_root) { system('bundle exec rake setup') }
+    pid = Process.spawn("bundle exec rails server --port #{port}", chdir: "#{rails_root}")
+    wait_for_response!("http://localhost:#{port}")
+    pid
+  end
+
+  private
+
+  # Waits up to 30 seconds for a response to root from the passed in url
+  def self.wait_for_response!(url)
+    connection = Faraday.new(url)
+    # Faraday retries are not behaving as expected, so we manually do so here.
+    i = 0
+    begin
+      connection.get('/')
+    rescue Faraday::Error::ConnectionFailed
+      sleep 1
+      retry if (i += 1)  < 30
+    end
+  end
 end
