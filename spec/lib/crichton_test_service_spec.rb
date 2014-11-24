cc326324a9e47d18f9802d59608f127e5ca8eb48
Connor Savage <csavage@mdsol.com>
2014-11-24 10:38:42 -0500

    check in spec for #initialize_rails! method

---
+++ spec/lib/crichton_test_service_spec.rb
@@ -0,0 +1,11 @@
+require 'crichton_test_service'
+
+RSpec.describe CrichtonTestService do
+  describe '#initialize_rails!' do
+    it 'loads the rails environment' do
+      expect { Rails }.to raise_error(NameError, "uninitialized constant Rails")
+      CrichtonTestService.initialize_rails!
+      expect { Rails }.not_to raise_error
+    end
+  end
+end
