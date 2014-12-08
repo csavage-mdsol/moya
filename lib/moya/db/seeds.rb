fc926160a2a3e4465a233a42aad24609864d33b8
Connor Savage <csavage@mdsol.com>
2014-12-08 15:26:35 -0500

    add simple seed script

--- lib/moya/db/seeds.rb
+++ lib/moya/db/seeds.rb
@@ -1,7 +1,13 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+5.times do |i|
+  Drd.create!(
+  uuid: SecureRandom.uuid,
+  name: "Pike#{i}",
+  status: Random.rand(2) > 0 ? 'activated' : 'deactivated',
+  kind: Random.rand(2) > 0 ? 'standard' : 'sentinel',
+  leviathan_uuid: leviathan_uuid = SecureRandom.uuid,
+  leviathan_url: "http://farscape.example.org/leviathan/#{leviathan_uuid}"
+  )
+end
