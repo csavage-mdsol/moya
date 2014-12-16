cbb1eab30373e22d86955b07bb4a6d1c79895b18
Connor Savage <csavage@mdsol.com>
2014-12-16 18:35:59 -0500

    add activate deactivate and destroy

--- lib/moya/app/controllers/drds_controller.rb
+++ lib/moya/app/controllers/drds_controller.rb
@@ -41,6 +41,27 @@ class DrdsController < ApplicationController
     respond_with(@drd, options)
   end
 
+  def activate
+    @drd = get_drd
+    @drd.activate!
+
+    respond_with(@drd, options)
+  end
+
+  def deactivate
+    @drd = get_drd
+    @drd.deactivate!
+
+    respond_with(@drd, options)
+  end
+
+  def destroy
+    @drd = get_drd
+    @drd.destroy!
+
+    respond_with(@drd, options)
+  end
+
   private
 
   # NB: Allowing a requester to directly manipulate options is not normal.  It is a convenience for testing.
@@ -54,6 +75,10 @@ class DrdsController < ApplicationController
 
   def drd_params
     params.require(:drd).permit(:name, :status, :kind, :leviathan_uuid, :leviathan_url)
+  end
+
+  def get_drd
+    Drd.find(UUIDTools::UUID.parse(params[:id]))
   end
 
 end
