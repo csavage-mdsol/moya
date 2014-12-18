fa23973c50a3a3a5eb165d749423a3c8d2a65cf5
Connor Savage <csavage@mdsol.com>
2014-12-18 10:26:50 -0500

    change kind from Roll-e to standard

--- spec/integration/test_service_spec.rb
+++ spec/integration/test_service_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe Moya do
       let(:drd_hash) {
           { drd: { name: 'Pike',
             status: 'activated',
-            kind: 'Roll-e',
+            kind: 'standard',
             leviathan_uuid: 'd34c78bd-583c-4eff-a66c-cd9b047417b4',
             leviathan_url: 'http://example.org/leviathan/d34c78bd-583c-4eff-a66c-cd9b047417b4'
           }
