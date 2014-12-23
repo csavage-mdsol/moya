f833b32cf50346ea89739ffa922b546b5ad1ac05
Connor Savage <csavage@mdsol.com>
2014-12-23 10:22:27 -0500

    remove goofy super initialize

--- lib/moya/lib/hyper_error.rb
+++ lib/moya/lib/hyper_error.rb
@@ -4,10 +4,6 @@ class HyperError < Crichton::Tools::BaseErrors
   include Crichton::Representor::State
   represents :hyper_error
 
-  def initialize(data)
-    super(data)
-  end
-
   def describes_url
     controller.request.original_url
   end
