2a873d72150b1303eda3969fee478c482f8ead20
Connor Savage <csavage@mdsol.com>
2014-12-24 12:58:22 -0500

    move a bunch of lets into a shared context

--- spec/integration/test_service_spec.rb
+++ spec/integration/test_service_spec.rb
@@ -3,30 +3,7 @@ RSpec.describe Moya do
   # rails process for each one.
   context "When the service is running" do
     context "When requesting hale json" do
-      let(:can_do_hash) { {conditions: ['can_do_anything']} }
-      let(:create_url) { "#{get_transition_uri(drds, 'create')}.hale_json" }
-      let(:drd_properties) { [ 'id',
-                               'name',
-                               'status',
-                               'old_status',
-                               'kind',
-                               'size',
-                               'leviathan_uuid',
-                               'created_at',
-                               'location',
-                               'location_detail',
-                               'destroyed_status'
-                             ] }
-      let(:error_properties) { ['details', 'error_code', 'http_status', 'stack_trace', 'title'] }
-      let(:drd_hash) {
-          { drd: { name: 'Pike',
-            status: 'activated',
-            kind: 'standard',
-            leviathan_uuid: 'd34c78bd-583c-4eff-a66c-cd9b047417b4',
-            leviathan_url: 'http://example.org/leviathan/d34c78bd-583c-4eff-a66c-cd9b047417b4'
-          }
-        }
-      }
+      include_context 'shared drd hale context'
 
       it 'responds appropriately to root' do
         expect(get('/').status).to eq(200)
